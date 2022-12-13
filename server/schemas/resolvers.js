375de879bb0df699888e59a44914db633dad26f5
Andy Kaminski <andy.kaminski42@gmail.com>
2022-12-13 18:18:27 -0500

    login mutation added to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,9 +25,22 @@ const resolvers = {
             }
             const token = signToken(user);
             return {token, user};
-        }
+        },
         // login
+        login: async (parent, { email, password }, context, info) => {
+            const user = await User.findOne({email});
+            if (!user) {
+                throw new AuthenticationError("Can't find this user or password");
+            }
 
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError("Can't find this user or password");
+            }
+            
+            const token = signToken(user);
+            return {token, user};
+        }
         // saveBook
 
         // removeBook
